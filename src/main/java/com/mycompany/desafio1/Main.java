109901dc9c987062a621395c51be4de9b6880d3b
santiago <scastellano10@gmail.com>
2017-05-18 11:35:44 -0300

    branch 1

--- src/main/java/com/mycompany/desafio1/Main.java
+++ src/main/java/com/mycompany/desafio1/Main.java
@@ -10,6 +10,10 @@ package com.mycompany.desafio1;
  * @author santi
  */
 public class Main {
+    private int san=0;
+    public void santi5(){
+        System.out.println("jjdjdjdjd");
+    }
     public void sac(){
         System.out.println("santi");
         System.out.println("santi5");
